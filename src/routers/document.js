a4ef0e08510affd3b4fe907a81d7ac45878fbdc6
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2021-06-28 14:48:59 +0200

    Add documents intergration

--- src/routers/document.js
+++ src/routers/document.js
@@ -3,20 +3,20 @@ const jwt = require('jsonwebtoken')
 const multer = require('multer')
 const sharp = require('sharp')
 const bodyParser = require('body-parser')
-const auth  = require('../middleware/auth')
-const {documents} = require('../models/document')
+const auth = require('../middleware/auth')
+const { documents } = require('../models/document')
 
 const router = new express.Router()
 
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Upload document file config
 const upload = multer({
     limits: {
         fileSize: 50000000
     },
-    fileFilter(req, file, cb){
-        if(!file.originalname.toLowerCase().match(/\.pdf/)){
+    fileFilter(req, file, cb) {
+        if (!file.originalname.toLowerCase().match(/\.pdf/)) {
             return cb(new Error('Please upload a PDF file'))
         }
         cb(undefined, true)
@@ -24,7 +24,7 @@ const upload = multer({
 })
 
 // Create article reports route
-router.post('/documents/researchreport', urlencodedParser, upload.single('document'), async(req, res)=>{
+router.post('/documents/researchreport', urlencodedParser, upload.single('document'), async (req, res) => {
     try {
         const document = new documents({
             title: req.body.title,
@@ -41,7 +41,7 @@ router.post('/documents/researchreport', urlencodedParser, upload.single('docume
 })
 
 // Create opinion pieces route
-router.post('/documents/opinionpieces', urlencodedParser, upload.single('document'), async(req, res)=>{
+router.post('/documents/opinionpieces', urlencodedParser, upload.single('document'), async (req, res) => {
     try {
         const document = new documents({
             title: req.body.title,
@@ -51,21 +51,21 @@ router.post('/documents/opinionpieces', urlencodedParser, upload.single('documen
         })
 
         await document.save()
-        res.status(200).send()
+        res.redirect(req.get('referer'))
     } catch (error) {
         res.status(500).send(error.message)
     }
 })
 
-router.get('/documents', async (req, res)=>{
+router.get('/documents', async (req, res) => {
     const match = {}
     let sort = {}
 
-    if(req.query.type){
+    if (req.query.type) {
         match.type = req.query.type
     }
 
-    if(req.query.sortBy){
+    if (req.query.sortBy) {
         const parts = req.query.sortBy.split(':')
         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
@@ -87,17 +87,17 @@ router.get('/documents', async (req, res)=>{
 })
 
 // Create article & workshop papers route
-router.post('/documents/articleworkshop', urlencodedParser, upload.single('document'), async(req, res)=>{
+router.post('/documents/articleworkshop', urlencodedParser, upload.single('document'), async (req, res) => {
     try {
         const document = new documents({
             title: req.body.title,
-            type: "Article & Workshop Papers",
+            type: "Article Workshops",
             abstract: req.body.abstract,
             file: req.file.buffer
         })
 
         await document.save()
-        res.status(200).send()
+        res.redirect(req.get('referer'))
     } catch (error) {
         res.status(500).send(error.message)
     }
@@ -108,23 +108,23 @@ router.get('/document/file/:id', async (req, res) => {
     try {
         const document = await documents.findById(req.params.id)
 
-        if(!document || !document.file){
+        if (!document || !document.file) {
             return res.status(400).send()
         }
 
         res.set('Content-Type', 'application/pdf')
         res.send(document.file)
-    }catch(error){
+    } catch (error) {
         res.status(500).send(error.message)
     }
 })
 
 // delete Document
-router.delete('/delete/:id', auth, async (req, res)=>{
+router.delete('/delete/:id', auth, async (req, res) => {
     try {
         const document = await documents.findById(req.params.id)
 
-        if(!document) res.status(404).send()
+        if (!document) res.status(404).send()
         await document.remove()
         res.status(200).send(document)
     } catch (error) {
